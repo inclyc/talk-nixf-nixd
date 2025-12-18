b1c4d5862848ef92324d7dba5eae6fee80550d85
Yingchi Long <longyingchi24s@ict.ac.cn>
2025-12-19 00:10:06 +0800

    don't use react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
 
@@ -7,8 +6,4 @@ if (!root) {
 	throw new Error("Root element with id 'root' was not found.");
 }
 
-ReactDOM.createRoot(root).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-);
+ReactDOM.createRoot(root).render(<App />);
