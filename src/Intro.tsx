e3b1b1feccae8b56f17c5286d2b908452823f6ca
Yingchi Long <longyingchi24s@ict.ac.cn>
2025-12-18 18:28:46 +0800

    Intro 第一页，或许应该把案例拆开成多页

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -3,17 +3,26 @@ export function Intro() {
 		<section>
 			<h3>能为我们带来什么？</h3>
 			<hr className="pb-4" />
-			<ol className="text-2xl">
+			<ul className="text-2xl">
+				{/* TODO: 或许作为单个的页面更好 */}
 				<li>
-					最开始的动机：在 <code>with pkgs; [ ]</code> 的里面，补全包名/版本号{" "}
-					<br />
-					Nix 作为一个
-					<strong className="underline underline-offset-4">包管理器</strong>,
-					用户能直接获得包名和版本号的补全是最基本的要求！
+					补全 <code>with pkgs; [ ]</code> 里面，包名/版本号 <br />
+					<span className="text-[0.8em]">
+						Nix 作为一个
+						<strong className="underline">包管理器</strong>,
+						用户能直接获得包名和版本号的补全是最基本的要求！
+					</span>
 				</li>
-				<li>NixOS 配置文件，能告诉我 options 的类型和可选值</li>
-				<li>在编辑 Nix 代码时，能获得语义检查和代码补全</li>
-			</ol>
+				<li>
+					编辑 NixOS 配置文件，能获得：
+					<u>options 的类型和可选值</u>
+				</li>
+				<li>
+					在编辑 Nix 代码时，能获得：
+					<u>语义检查</u>和<u>代码补全</u>
+				</li>
+				<li>上述能力静态检查集成到 Nixpkgs CI 流程中，避免人肉 parser</li>
+			</ul>
 		</section>
 	);
 }
